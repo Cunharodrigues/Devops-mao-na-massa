019407b693d07c2f482e86e67c1389379b38338b
unknown <cunharodrigues>
2023-12-06 15:45:58 -0300

    first commit - lab Vagrant

---
+++ vagrant-lab/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"9c9f843a49cfdd60f3c68c2f34b5d60f094db26229e45b00d03d23b015d342cd","vagrant_version":"2.4.0"}
